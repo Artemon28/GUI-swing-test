a92d0d8f4bd7d8a0dc3888c4ab1237e629f67e27
Artemiy <los28.2001@mail.ru>
2022-06-07 17:15:08 +0300

    Menu and dialogs Menu with buttons exit and express report report and express reports show bug in dialog window

--- src/Main/GUI.java
+++ src/Main/GUI.java
@@ -7,28 +7,33 @@ import javax.swing.*;
 public class GUI extends JFrame {
 	
 	private JButton sendButton = new JButton("send text");
-	private JTextField input = new JTextField("", 5);
-	private JLabel label = new JLabel("Input: ");
+	private JTextField username = new JTextField(10);
+	private JLabel label = new JLabel("Insert your name: ");
+	private JTextArea problemDescription = new JTextArea(5, 20);
 	
 	public GUI() {
-		super ("first try");
-		this.setBounds(100, 100, 250, 100);
+		super ("Bug Report");
+		this.setBounds(100, 100, 400, 300);
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		Container container = this.getContentPane();
-		container.setLayout(new GridLayout(3, 2, 2, 2));
+		Container container = this.getContentPane();		
+		container.setLayout(new FlowLayout ());
 		container.add(label);
-		container.add(input);
-		sendButton.addActionListener(new ButtonEventListener());		
+		container.add(username);
+		container.add(new JLabel("if you have problems, write it here: "));
+		problemDescription.setLineWrap(true);
+		JScrollPane jScrollPane = new JScrollPane(problemDescription);
+		container.add(jScrollPane);
+		sendButton.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				String message = "";
+			message += "user: " + username.getText() + " has problem: " + '\n';
+			message += problemDescription.getText();
+			JOptionPane.showMessageDialog(null, message, "Output", JOptionPane.PLAIN_MESSAGE);
+			}
+		});
 		container.add(sendButton);
-		
-		
 	}
 	
-	class ButtonEventListener implements ActionListener {
-		public void actionPerformed (ActionEvent e) {
-			String message = "";
-			message += input.getText();
-			JOptionPane.showMessageDialog(null, message, "Output", JOptionPane.PLAIN_MESSAGE);
-		}
-	}
+	
 }
--- src/Main/Main.java
+++ src/Main/Main.java
@@ -1,9 +1,44 @@
 package Main;
 
+import java.awt.Container;
+import java.awt.FlowLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JLabel;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
+import javax.swing.JOptionPane;
+import javax.swing.JScrollPane;
+
 public class Main {
 
 	public static void main(String[] args) {
 		GUI app = new GUI();
 		app.setVisible(true);
+		
+		JMenuBar jMenuBar = new JMenuBar();
+		JMenu options = new JMenu("Options");
+		jMenuBar.add(options);
+		
+		options.add(new JMenuItem("express problem")).addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				String problemText = JOptionPane.showInputDialog("insert you problem here");
+				if (problemText != null && problemText.length() > 0) {
+					JOptionPane.showMessageDialog(null, problemText, "Your problem", JOptionPane.PLAIN_MESSAGE);
+				}
+			}
+		});
+		options.add(new JMenuItem("Exit")).addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				System.exit(0);
+			}
+		});
+		
+		app.setJMenuBar(jMenuBar);
+		app.revalidate();
 	}
 }
